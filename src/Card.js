ece63951386998e5f5819f7b13f0825f6fece1f3
Andrew Worley <andrew.worley@johnlewis.co.uk>
2018-06-07 13:40:23 +0100

    Convert Card to functional component and fix the placeholder text

--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,13 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class Card extends Component {
-    render() {
-        return (<div className="card">
-            <p className="card-id">#{this.props.cardId}</p>
-            <textarea className="card-editor"
-                   placeholder="Please enter a title..."
-                   value={this.props.content}
-                   onChange={(e) => this.props.onChange(e, this.props.cardId)}/>
-        </div>);
-    }
+function Card(props) {
+    return (<div className="card">
+        <p className="card-id">#{props.cardId}</p>
+        <textarea className="card-editor"
+                  placeholder="Please enter something..."
+                  value={props.content}
+                  onChange={(e) => props.onChange(e, props.cardId)}/>
+    </div>);
 }
 
 export default Card;
